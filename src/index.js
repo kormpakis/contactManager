478ee0ec61ee26274dcf90167dfb2b1570108483
Giorgos Kormpakis <korbakis@synelixis.com>
2020-09-14 10:31:16 +0300

    Redux state persistor provided.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/store";
+import {store, persistedStore} from "./redux/store";
 import {Provider} from 'react-redux'
+import {PersistGate} from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
+    <PersistGate loading={null} persistor={persistedStore}>
       <App/>
-    </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,20 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import contactReducer from "./contacts/reducer";
 import thunk from "redux-thunk";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(combineReducers({
+const rootReducer = combineReducers({
   contacts: contactReducer
-}), applyMiddleware(thunk));
+});
 
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: 'contacts'
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk))
+export const persistedStore = persistStore(store)
